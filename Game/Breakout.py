6dec820e2bf9a3c324aac1cefccfb766d1b29db0
mxm-nclv <maxim.nclv@gmail.com>
2021-01-22 19:23:28 -0700

    Added more to Breakout Constructor

--- Game/Breakout.py
+++ Game/Breakout.py
@@ -10,8 +10,35 @@ class Breakout:
         self.__lives = 5
         self.__score = 0
 
-        self.__level = Level()
+        self.__level = Level(self)
         self.__level.load(0)
+
+        self.__pad = Pad((0,0), 0)
+        self.__balls = [
+            Ball((0, 0), 0, self)
+        ]
+
+        pygame.init()
+        pygame.mixer.init()
+        pygame.display.set_caption("Game Programing With Python & Pygame")
+
+        self.__clock = pygame.time.Clock()
+
+        self.screen = pygame.display.set_mode(GameConstants.SCREEN_SIZE,
+                                              pygame.DOUBLEBUF, 32)
+
+        pygame.mouse.set_visible(0)
+
+        self.__scenes = (
+            PlayingGameScene(self),
+            GameOverScene(self),
+            HighscoreScene(self),
+            MenuScene(self)
+        )
+
+        self.__currentScene = 0
+
+        self.__sound = ()
 
     def start(self):
         pass
